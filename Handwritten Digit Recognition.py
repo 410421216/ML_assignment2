41ce45cbd78371d1193dea5b695357d3a5edc533
蕭子渝 <tommy961024@gmail.com>
2018-06-29 07:53:51 +0800

    create model and then train it. screenshot the result.

--- Handwritten Digit Recognition.py
+++ Handwritten Digit Recognition.py
@@ -9,13 +9,16 @@ from __future__ import print_function
 
 import keras
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import RMSprop
 
 #查看mnist資料型態
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
-print(y_test.shape)
+#print(x_train.shape)
+#print(y_train.shape)
+#print(x_test.shape)
+#print(y_test.shape)
 
 #reducing dimension & processing character image
 x_train = x_train.reshape(60000, 784)
@@ -24,6 +27,28 @@ x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
+#print(x_train.shape)
+#print(x_test.shape)
 
-print(x_train.shape)
-print(x_test.shape)
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=128,
+                    epochs=10,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
